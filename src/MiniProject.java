52c683d0b6842563887b8f72383051a27bcf8389
Shanzida-2001 <haque2305101745@diu.edu.bd>
2024-11-22 01:02:46 +0600

    Third Commit

--- src/MiniProject.java
+++ src/MiniProject.java
@@ -1,6 +1,31 @@
 
 public class MiniProject {
+
     public static void main(String[] args) {
-        
+
     }
+}
+
+class Garment {
+
+}
+
+class Fabric {
+
+}
+
+class Supplier {
+
+}
+
+class Order {
+
+}
+
+class Customer {
+
+}
+
+class Inventory {
+
 }
