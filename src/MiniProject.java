63b2ae119fe6c48508b2db9dadca07a9b01e4fc3
Shanzida-2001 <haque2305101745@diu.edu.bd>
2024-11-22 01:04:22 +0600

    4th Commit

--- src/MiniProject.java
+++ src/MiniProject.java
@@ -8,6 +8,17 @@ public class MiniProject {
 
 class Garment {
 
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+
+    void updateStock(int quantity) {
+        this.stockQuantity = quantity;
+    }
 }
 
 class Fabric {
