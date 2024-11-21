bdb370e10c716e0d99049f3cf5bc0271a5db6b0f
Shanzida-2001 <haque2305101745@diu.edu.bd>
2024-11-22 01:27:41 +0600

    12th Commit

--- src/MiniProject.java
+++ src/MiniProject.java
@@ -78,6 +78,17 @@ class Order {
         return totalAmount;
     }
 
+    void printOrderDetails() {
+        System.out.println("--------------------------");
+        System.out.println("Order Details");
+        System.out.println("--------------------------");
+        for (Garment g : garments) {
+            System.out.println("Name: " + g.name);
+            System.out.println("Price: " + g.price);
+            System.out.println("Description: " + g.description);
+            System.out.println("--------------------------");
+        }
+    }
 }
 
 class Customer {
